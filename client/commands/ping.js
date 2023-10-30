0054894bce11782783a20571c130f03793f5879a
aroary <me@aroary.com>
2023-10-30 19:10:47 -0400

    update ping command

--- client/commands/ping.js
+++ client/commands/ping.js
@@ -5,11 +5,15 @@ const command = new SlashCommandBuilder()
     .setDescription("Ping the bot");
 
 /**
- * 
  * @param {CommandInteraction} interaction 
  */
 function handle(interaction) {
-    interaction.reply({ content: "Pong", ephemeral: true });
+    interaction
+        .reply({ content: "Pong", ephemeral: true })
+        .then(reply => reply
+            .edit({ body: `${interaction.createdTimestamp - reply.createdTimestamp}ms` })
+            .catch(console.log))
+        .catch(console.log);
 }
 
 module.exports = { command, call: handle };
