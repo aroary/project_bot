99177c5aa6a1c7ec92690f055a001765cb71b084
aroary <me@aroary.com>
2023-10-31 03:47:53 -0400

    prevent spammage

--- client/events/typingStart.js
+++ client/events/typingStart.js
@@ -1,11 +1,13 @@
 const { Events, Typing, ActivityType } = require("discord.js");
 
 var delay;
+var last;
 
 /**
  * @param {Typing} typing
  */
 function handle(typing) {
+    if (typing.user.id !== last) {
     const client = require("../client");
 
     client.user.setPresence({
@@ -19,6 +21,8 @@ function handle(typing) {
     }), 10000);
     else delay.refresh();
 
+        last = typing.user.id;
+    }
 }
 
 module.exports = { event: Events.TypingStart, call: handle };
