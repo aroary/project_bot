4dfc5e6e890558bc349a2fe26483271197e8ac94
aroary <me@aroary.com>
2023-11-03 17:50:46 -0400

    use error util

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,17 +11,10 @@ function deploy(commands) {
 }
 
 if (require.main === module) {
+    require("../utils/error")();
     require('dotenv').config();
 
     const fs = require("fs");
-
-    // Set error report directory
-    fs.mkdirSync("../logs", { recursive: true });
-    for (const file of fs.readdirSync("../logs")) fs.unlinkSync("../logs/" + file);
-    process.report.directory = "../logs";
-    process.report.reportOnFatalError = true;
-    process.report.reportOnSignal = true;
-    process.report.reportOnUncaughtException = true;
 
     if (process.argv.includes("--reset")) reset
         .then(() => console.log("Client:", "Deployed"))
