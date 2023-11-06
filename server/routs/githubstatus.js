69e7e90ab5d8ae194491b053e146f7c26bd12bb8
aroary <me@aroary.com>
2023-11-06 06:06:21 -0500

    ugh. status changes

--- server/routs/githubstatus.js
+++ server/routs/githubstatus.js
@@ -8,30 +8,40 @@ const Webhook = require("../../utils/webhook");
  */
 function handle(req, res) {
     try {
-        var body = [];
-        req
-            .on('data', chunk => body.push(chunk))
-            .on('end', () => body = JSON.parse(Buffer.concat(body).toString()))
-            .on("error", process.report.writeReport);
-
-        const embed = new EmbedBuilder()
-            .setTitle(body.component.name)
-            .setDescription(body.page.status_description)
-            .setURL(`https://www.githubstatus.com/incidents/${body.page.id}`)
-            .addFields(
-                { name: "Old Status", value: body.component_update.old_status.replace("_", " "), inline: false },
-                { name: "New Status", value: body.component_update.new_status.replace("_", " "), inline: false }
-            )
-            .setColor({ none: "#28a746", minor: "#dbab09", major: "#e36209", critical: "#dc3546" }[body.page.status_indicator])
-            .setTimestamp();
-
-        new Webhook(process.env["STATUS_ID"], process.env["STATUS_TOKEN"])
-            .post({ embeds: [embed.toJSON()] })
-            .then(request => console.log("Webhook:", request.statusCode, request.statusMessage))
+        new Promise((resolve, reject) => {
+            var body = [];
+            req
+                .on('data', chunk => body.push(chunk))
+                .on('end', () => resolve(JSON.parse(Buffer.concat(body).toString())))
+                .on("error", reject);
+        })
+            .then(body => new Webhook(process.env["STATUS_ID"], process.env["STATUS_TOKEN"])
+                .post({
+                    embeds: [new EmbedBuilder()
+                        .setTitle(body.component.name)
+                        .setDescription(body.page.status_description)
+                        .setURL(`https://www.githubstatus.com/incidents/${body.page.id}`)
+                        .addFields(
+                            {
+                                name: "Old Status",
+                                value: body.component_update.old_status.replace("_", " "),
+                                inline: false
+                            },
+                            {
+                                name: "New Status",
+                                value: body.component_update.new_status.replace("_", " "),
+                                inline: false
+                            }
+                        )
+                        .setColor({ none: "#28a746", minor: "#dbab09", major: "#e36209", critical: "#dc3546" }[body.page.status_indicator])
+                        .setTimestamp()
+                        .toJSON()]
+                })
+                .then(request => console.log("Webhook:", request.statusCode, request.statusMessage))
+                .catch(process.report.writeReport))
             .catch(process.report.writeReport);
-
     } catch (error) {
-        console.log(error);
+        process.report.writeReport(error);
     } finally {
         res.writeHead(200, ":)").end();
     }
