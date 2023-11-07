3cfd66544a0cac0d4c8d351bf0a12c90171d05b9
aroary <me@aroary.com>
2023-11-07 07:16:14 -0500

    patch

--- utils/advisories.js
+++ utils/advisories.js
@@ -3,6 +3,7 @@ const { request } = require("@octokit/request");
 const Webhook = require("./webhook");
 
 var latest = null;
+var published = new Set();
 
 /**
  * @description Start watching advisories
@@ -11,15 +12,16 @@ var latest = null;
 function initiate() {
     return new Promise((resolve, reject) => request("GET /advisories")
         .then(response => {
-            latest = response.data[0].summary;
+            latest = response.data[0].ghsa_id;
 
             resolve(setInterval(() => request("GET /advisories")
                 .then(res => {
-                    var index = res.data.findIndex(item => item.summary === latest);
+                    var index = res.data.findIndex(item => item.ghsa_id === latest);
                     if (index < 0) index = res.data.length;
 
                     if (index) res.data
                         .slice(0, index)
+                        .filter(item => !published.has(item.ghsa_id))
                         .reverse()
                         .map(item => new EmbedBuilder()
                             .setTitle(item.summary)
@@ -36,7 +38,10 @@ function initiate() {
                             .then(request => console.log("Webhook:", request.statusCode, request.statusMessage))
                             .catch(process.report.writeReport), i * 5000));
 
-                    latest = res.data[0].summary;
+                    latest = res.data[0].ghsa_id;
+                    res.data.forEach(item => published.add(item.ghsa_id));
+
+                    if (published.size > 60) published = new Set([...published].slice(30))
                 })
                 .catch(process.report.writeReport), 300000 /* five minutes */));
         })
