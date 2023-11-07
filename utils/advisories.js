d4b7c2529978be938ce9a434915023394bd43fa6
aroary <me@aroary.com>
2023-11-07 07:44:30 -0500

    complete migration

--- utils/advisories.js
+++ utils/advisories.js
@@ -2,7 +2,6 @@ const { EmbedBuilder } = require("discord.js");
 const { request } = require("@octokit/request");
 const Webhook = require("./webhook");
 
-var latest = null;
 var published = new Set();
 
 /**
@@ -12,15 +11,11 @@ var published = new Set();
 function initiate() {
     return new Promise((resolve, reject) => request("GET /advisories")
         .then(response => {
-            latest = response.data[0].ghsa_id;
+            published = new Set(response.data.map(item => item.ghsa_id));
 
             resolve(setInterval(() => request("GET /advisories")
                 .then(res => {
-                    var index = res.data.findIndex(item => item.ghsa_id === latest);
-                    if (index < 0) index = res.data.length;
-
                     if (index) res.data
-                        .slice(0, index)
                         .filter(item => !published.has(item.ghsa_id))
                         .reverse()
                         .map(item => new EmbedBuilder()
@@ -38,7 +33,6 @@ function initiate() {
                             .then(request => console.log("Webhook:", request.statusCode, request.statusMessage))
                             .catch(process.report.writeReport), i * 5000));
 
-                    latest = res.data[0].ghsa_id;
                     res.data.forEach(item => published.add(item.ghsa_id));
 
                     if (published.size > 60) published = new Set([...published].slice(30))
