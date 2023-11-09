b7a4e80c4e29abd4ae36d0082b537ac0fcf2ce23
aroary <me@aroary.com>
2023-11-09 04:19:11 -0500

    allow context commands

--- client/events/interactionCreate.js
+++ client/events/interactionCreate.js
@@ -6,7 +6,7 @@ const { Events, Interaction } = require("discord.js");
 function handle(interaction) {
     const client = require("../client");
 
-    if (interaction.isChatInputCommand()) {
+    if (interaction.isChatInputCommand() || interaction.isMessageContextMenuCommand()) {
         const command = client.commands.get(interaction.commandName);
         if (command) command.call(interaction);
         else interaction.reply({ content: "Something whent worng", ephemeral: true });
