19d6764697a4f733b289faa4338a901604a4c9a5
aroary <me@aroary.com>
2023-11-12 19:14:52 -0500

    env tests

--- scripts/test.js
+++ scripts/test.js
@@ -58,3 +58,32 @@ fs
         assert(value.routs.length, name);
         assert(typeof value.call === "function", name);
     });
+
+const env = {
+    ...process.env,
+    ...require('dotenv').parse(fs.readFileSync(path.join(__dirname, "../.env")))
+};
+
+assert(env["PORT"], "PORT not configured.");
+assert(env["BOT_TOKEN"], "BOT_TOKEN not configured.");
+assert(env["BOT_APP_ID"], "BOT_APP_ID not configured.");
+assert(env["BOT_PUBLIC_KEY"], "BOT_PUBLIC_KEY not configured.");
+assert(env["BOT_CLIENT_SECRET"], "BOT_CLIENT_SECRET not configured.");
+assert(env["DM_ID"], "DM_ID not configured.");
+assert(env["DM_TOKEN"], "DM_TOKEN not configured.");
+assert(env["CHANGELOG_ID"], "CHANGELOG_ID not configured.");
+assert(env["CHANGELOG_TOKEN"], "CHANGELOG_TOKEN not configured.");
+assert(env["STATUS_ID"], "STATUS_ID not configured.");
+assert(env["STATUS_TOKEN"], "STATUS_TOKEN not configured.");
+assert(env["ADVISOR_ID"], "ADVISOR_ID not configured.");
+assert(env["ADVISOR_TOKEN"], "ADVISOR_TOKEN not configured.");
+assert(env["SERVER_ID"], "SERVER_ID not configured.");
+assert(env["REPOSITORIES_CHANNEL_ID"], "REPOSITORIES_CHANNEL_ID not configured.");
+// assert(env["GENERAL_CHANNEL_ID"], "GENERAL_CHANNEL_ID not configured.");
+// assert(env["BOOSTER_ROLE_ID"], "BOOSTER_ROLE_ID not configured.");
+// assert(env["OPENAI_API_KEY"], "ADVISOR_TOKEN not configured.");
+assert(env["DB_USERNAME"], "DB_USERNAME not configured.");
+assert(env["DB_PASSWORD"], "DB_PASSWORD not configured.");
+assert(env["DB_SERVER"], "DB_SERVER not configured.");
+assert(env["DB_PORT"], "DB_PORT not configured.");
+assert(env["DB_NAME"], "DB_NAME not configured.");
